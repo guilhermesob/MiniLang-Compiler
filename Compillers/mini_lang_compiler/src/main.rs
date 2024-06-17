0e70da0cd2c8ff4811cec0a3d92d40b217c49e38
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2024-06-17 01:20:07 +0000

    feat add, cursos.a nd logig ,ok

--- Compillers/mini_lang_compiler/src/main.rs
+++ Compillers/mini_lang_compiler/src/main.rs
@@ -1,4 +1,31 @@
 #[derive(Debug, PartialEq)]
+enum AstNode {
+    Number(f64),
+    Identifier(String),
+    BinaryOp {
+        op: BinaryOperator,
+        left: Box<AstNode>,
+        right: Box<AstNode>,
+    },
+}
+
+#[derive(Debug, PartialEq)]
+enum BinaryOperator {
+    Plus,
+    Minus,
+    Multiply,
+    Divide,
+    Equal,
+    NotEqual,
+    LessThan,
+    GreaterThan,
+    LessThanOrEqual,
+    GreaterThanOrEqual,
+    And,
+    Or,
+}
+
+#[derive(Debug, PartialEq)]
 enum Token {
     Number(f64),
     Plus,
@@ -16,6 +43,210 @@ enum Token {
     GreaterThanOrEqual,
     And,
     Or,
+}
+
+#[derive(Debug)]
+enum ParseError {
+    UnexpectedToken,
+    UnexpectedEndOfInput,
+}
+
+struct Parser<'a> {
+    tokens: &'a [Token],
+    position: usize,
+}
+
+impl<'a> Parser<'a> {
+    fn new(tokens: &'a [Token]) -> Self {
+        Self { tokens, position: 0 }
+    }
+
+    fn parse(&mut self) -> Result<AstNode, ParseError> {
+        self.parse_expression()
+    }
+
+    fn parse_expression(&mut self) -> Result<AstNode, ParseError> {
+        self.parse_logical_or()
+    }
+
+    fn parse_logical_or(&mut self) -> Result<AstNode, ParseError> {
+        let mut node = self.parse_logical_and()?;
+
+        while self.match_token(&Token::Or) {
+            self.position += 1;
+            let right = self.parse_logical_and()?;
+            node = AstNode::BinaryOp {
+                op: BinaryOperator::Or,
+                left: Box::new(node),
+                right: Box::new(right),
+            };
+        }
+
+        Ok(node)
+    }
+
+    fn parse_logical_and(&mut self) -> Result<AstNode, ParseError> {
+        let mut node = self.parse_equality()?;
+
+        while self.match_token(&Token::And) {
+            self.position += 1;
+            let right = self.parse_equality()?;
+            node = AstNode::BinaryOp {
+                op: BinaryOperator::And,
+                left: Box::new(node),
+                right: Box::new(right),
+            };
+        }
+
+        Ok(node)
+    }
+
+    fn parse_equality(&mut self) -> Result<AstNode, ParseError> {
+        let mut node = self.parse_comparison()?;
+
+        while self.match_token(&Token::Equal) || self.match_token(&Token::NotEqual) {
+            let op = if self.match_token(&Token::Equal) {
+                self.position += 1;
+                BinaryOperator::Equal
+            } else {
+                self.position += 1;
+                BinaryOperator::NotEqual
+            };
+            let right = self.parse_comparison()?;
+            node = AstNode::BinaryOp {
+                op,
+                left: Box::new(node),
+                right: Box::new(right),
+            };
+        }
+
+        Ok(node)
+    }
+
+    fn parse_comparison(&mut self) -> Result<AstNode, ParseError> {
+        let mut node = self.parse_term()?;
+
+        while self.match_token(&Token::LessThan)
+            || self.match_token(&Token::GreaterThan)
+            || self.match_token(&Token::LessThanOrEqual)
+            || self.match_token(&Token::GreaterThanOrEqual)
+        {
+            let op = if self.match_token(&Token::LessThan) {
+                self.position += 1;
+                BinaryOperator::LessThan
+            } else if self.match_token(&Token::GreaterThan) {
+                self.position += 1;
+                BinaryOperator::GreaterThan
+            } else if self.match_token(&Token::LessThanOrEqual) {
+                self.position += 1;
+                BinaryOperator::LessThanOrEqual
+            } else {
+                self.position += 1;
+                BinaryOperator::GreaterThanOrEqual
+            };
+            let right = self.parse_term()?;
+            node = AstNode::BinaryOp {
+                op,
+                left: Box::new(node),
+                right: Box::new(right),
+            };
+        }
+
+        Ok(node)
+    }
+
+    fn parse_term(&mut self) -> Result<AstNode, ParseError> {
+        let mut node = self.parse_factor()?;
+
+        while self.match_token(&Token::Plus) || self.match_token(&Token::Minus) {
+            let op = if self.match_token(&Token::Plus) {
+                self.position += 1;
+                BinaryOperator::Plus
+            } else {
+                self.position += 1;
+                BinaryOperator::Minus
+            };
+            let right = self.parse_factor()?;
+            node = AstNode::BinaryOp {
+                op,
+                left: Box::new(node),
+                right: Box::new(right),
+            };
+        }
+
+        Ok(node)
+    }
+
+    fn parse_factor(&mut self) -> Result<AstNode, ParseError> {
+        let mut node = self.parse_unary()?;
+
+        while self.match_token(&Token::Multiply) || self.match_token(&Token::Divide) {
+            let op = if self.match_token(&Token::Multiply) {
+                self.position += 1;
+                BinaryOperator::Multiply
+            } else {
+                self.position += 1;
+                BinaryOperator::Divide
+            };
+            let right = self.parse_unary()?;
+            node = AstNode::BinaryOp {
+                op,
+                left: Box::new(node),
+                right: Box::new(right),
+            };
+        }
+
+        Ok(node)
+    }
+
+    fn parse_unary(&mut self) -> Result<AstNode, ParseError> {
+        if self.match_token(&Token::Minus) {
+            self.position += 1;
+            let expr = self.parse_primary()?;
+            return Ok(AstNode::BinaryOp {
+                op: BinaryOperator::Minus,
+                left: Box::new(AstNode::Number(0.0)),
+                right: Box::new(expr),
+            });
+        }
+        self.parse_primary()
+    }
+
+    fn parse_primary(&mut self) -> Result<AstNode, ParseError> {
+        if let Some(token) = self.tokens.get(self.position) {
+            match token {
+                Token::Number(n) => {
+                    self.position += 1;
+                    Ok(AstNode::Number(*n))
+                }
+                Token::Identifier(id) => {
+                    self.position += 1;
+                    Ok(AstNode::Identifier(id.clone()))
+                }
+                Token::LeftParenthesis => {
+                    self.position += 1;
+                    let expr = self.parse_expression()?;
+                    if self.match_token(&Token::RightParenthesis) {
+                        self.position += 1;
+                        Ok(expr)
+                    } else {
+                        Err(ParseError::UnexpectedToken)
+                    }
+                }
+                _ => Err(ParseError::UnexpectedToken),
+            }
+        } else {
+            Err(ParseError::UnexpectedEndOfInput)
+        }
+    }
+
+    fn match_token(&self, token: &Token) -> bool {
+        if let Some(t) = self.tokens.get(self.position) {
+            t == token
+        } else {
+            false
+        }
+    }
 }
 
 fn tokenize(input: &str) -> Vec<Token> {
@@ -144,7 +375,9 @@ fn tokenize(input: &str) -> Vec<Token> {
 fn main() {
     let input = "3.14 + (5 * 7.2) - 2 / (1 + 1.1)";
     let tokens = tokenize(input);
-    println!("{:?}", tokens);
+    let mut parser = Parser::new(&tokens);
+    let ast = parser.parse();
+    println!("{:?}", ast);
 }
 
 #[cfg(test)]
@@ -263,5 +496,88 @@ mod tests {
             Token::Identifier("c".to_string()),
         ];
         assert_eq!(tokenize(input), expected);
+    }
+
+    #[test]
+    fn test_parse_simple_expression() {
+        let tokens = vec![
+            Token::Number(3.0),
+            Token::Plus,
+            Token::Number(5.0),
+            Token::Multiply,
+            Token::Number(7.0),
+        ];
+        let mut parser = Parser::new(&tokens);
+        let ast = parser.parse().unwrap();
+        let expected = AstNode::BinaryOp {
+            op: BinaryOperator::Plus,
+            left: Box::new(AstNode::Number(3.0)),
+            right: Box::new(AstNode::BinaryOp {
+                op: BinaryOperator::Multiply,
+                left: Box::new(AstNode::Number(5.0)),
+                right: Box::new(AstNode::Number(7.0)),
+            }),
+        };
+        assert_eq!(ast, expected);
+    }
+
+    #[test]
+    fn test_parse_expression_with_parentheses() {
+        let tokens = vec![
+            Token::Number(3.0),
+            Token::Plus,
+            Token::LeftParenthesis,
+            Token::Number(5.0),
+            Token::Multiply,
+            Token::Number(7.0),
+            Token::RightParenthesis,
+        ];
+        let mut parser = Parser::new(&tokens);
+        let ast = parser.parse().unwrap();
+        let expected = AstNode::BinaryOp {
+            op: BinaryOperator::Plus,
+            left: Box::new(AstNode::Number(3.0)),
+            right: Box::new(AstNode::BinaryOp {
+                op: BinaryOperator::Multiply,
+                left: Box::new(AstNode::Number(5.0)),
+                right: Box::new(AstNode::Number(7.0)),
+            }),
+        };
+        assert_eq!(ast, expected);
+    }
+
+    #[test]
+    fn test_parse_complex_expression() {
+        let tokens = vec![
+            Token::Number(3.0),
+            Token::Plus,
+            Token::Number(5.0),
+            Token::Multiply,
+            Token::Number(7.0),
+            Token::Minus,
+            Token::Number(2.0),
+            Token::Divide,
+            Token::Number(1.0),
+        ];
+        let mut parser = Parser::new(&tokens);
+        let ast = parser.parse().unwrap();
+        let expected = AstNode::BinaryOp {
+            op: BinaryOperator::Minus,
+            left: Box::new(AstNode::BinaryOp {
+                op: BinaryOperator::Plus,
+                left: Box::new(AstNode::Number(3.0)),
+                right: Box::new(AstNode::BinaryOp {
+                    op: BinaryOperator::Multiply,
+                    left: Box::new(AstNode::Number(5.0)),
+                    right: Box::new(AstNode::Number(7.0)),
+                }),
+            }),
+            right: Box::new(AstNode::BinaryOp {
+                op: BinaryOperator::Divide,
+                left: Box::new(AstNode::Number(2.0)),
+                right: Box::new(AstNode::Number(1.0)),
+            }),
+        };
+        assert_eq!(ast, expected);
     }
 }
